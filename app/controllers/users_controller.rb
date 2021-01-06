3c5645012a30fa4e95cf761b01a2307483fd53c5
Amir Seigal <100630342@student.swin.edu.au>
2021-01-06 18:37:13 +1100

    added feed to the home button

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,6 @@ class UsersController < ApplicationController
   end
 
   def feed 
-    
     @posts = Post.where(user_id: current_user.all_following.pluck(:id)).or(Post.where(user_id: current_user.id)).order(created_at: :DESC)
   end
 
