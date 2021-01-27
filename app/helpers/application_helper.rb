ee200d76716278edf5489f59ecb93089f3e7b899
Amir Seigal <100630342@student.swin.edu.au>
2021-01-27 19:01:22 +1100

    added input masking

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
 
     def user_avatar(user, size=40)
-        
         if user.avatar.attached?
             user.avatar.variant(resize: "#{size}x#{size}!")
         else
             gravatar_image_url('aseighali96@gmail.com'.gsub('spam', 'mdeering'),size: size )
         end
     end
+
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,15 +7,17 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require('packs/custom')
-require('packs/posts.js')
+//require('packs/custom')
+//require('packs/posts.js')
 //= require jquery
 //= require bootstrap-sprockets
 //= require rails-ujs
 //= require moment
 //= require moment/fr.js
+require("jquery-mask-plugin")
 
-//= require jquery.inputmask.bundle.min
+$.jMaskGlobals.watchDataMask = true;
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
