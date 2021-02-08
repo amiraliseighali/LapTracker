dcc31efd20224d6f51f6b53236025d9ead6e64a0
Amir Seigal <100630342@student.swin.edu.au>
2021-02-08 16:31:21 +1100

    solved avatar issue

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
 include Pagy::Frontend
 
-    def user_avatar(user, size=40)
+    def user_avatar(user)
         if user.avatar.attached?
-            user.avatar.variant(resize: "#{size}x#{size}!")
+            user.avatar
         else
             gravatar_image_url('aseighali96@gmail.com'.gsub('spam', 'mdeering'),size: size )
         end
